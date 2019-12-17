338b3f38e56bcb5756722ca99e16ddb841a0fec7
Javier Gonzalez <javierggt@yahoo.com>
2019-12-17 12:57:36 -0500

    Added changes to use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from setuptools import setup
 
-from Ska.DBI import __version__
-
 try:
     from testr.setup_helper import cmdclass
 except ImportError:
@@ -12,7 +10,8 @@ setup(name='Ska.DBI',
       author='Tom Aldcroft',
       description='Database interface utilities',
       author_email='taldcroft@cfa.harvard.edu',
-      version=__version__,
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
       zip_safe=False,
       packages=['Ska', 'Ska.DBI', 'Ska.DBI.tests'],
       package_data={'Ska.DBI.tests': ['ska_dbi_test_table.sql']},
