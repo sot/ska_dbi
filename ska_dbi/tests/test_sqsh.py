c0feecf19d055983752be9e850a629f42d1d1b5c
Jean Connelly <jconnelly@cfa.harvard.edu>
2024-02-02 10:38:13 -0500

    Set sqsh tests to skip not on_head_network

--- ska_dbi/tests/test_sqsh.py
+++ ska_dbi/tests/test_sqsh.py
@@ -1,6 +1,10 @@
+import pytest
+from testr.test_helper import on_head_network
 from ska_dbi.sqsh import Sqsh
+from ska_dbi.common import NoPasswordError
 
 
+@pytest.mark.skipif(not on_head_network, reason="Test only runs on HEAD network")
 def test_fetch_axafapstat_lines():
     s = Sqsh()
     query = "select * from aspect_1 where obsid=5438"
@@ -8,6 +12,7 @@ def test_fetch_axafapstat_lines():
     assert len(lines) >= 5
 
 
+@pytest.mark.skipif(not on_head_network, reason="Test only runs on HEAD network")
 def test_fetchall_axafapstat():
     s = Sqsh()
     query = "select * from aspect_1 where obsid=5438"
@@ -17,6 +22,7 @@ def test_fetchall_axafapstat():
     assert dat[dat["revision"] == 1]['ascdsver'] == '7.6.3'
 
 
+@pytest.mark.skipif(not on_head_network, reason="Test only runs on HEAD network")
 def test_fetchall_axafocat_empty():
     s = Sqsh(server='sqlsao', user='aca_ops', database='axafocat')
     query = "select * from target where obsid=80000"
@@ -24,6 +30,7 @@ def test_fetchall_axafocat_empty():
     assert len(dat) == 0
 
 
+@pytest.mark.skipif(not on_head_network, reason="Test only runs on HEAD network")
 def test_fetchone_axafapstat():
     s = Sqsh()
     query = "select * from aspect_1 where obsid=5438 and revision=1"
@@ -32,6 +39,7 @@ def test_fetchone_axafapstat():
     assert dat['ascdsver'] == '7.6.3'
 
 
+@pytest.mark.skipif(not on_head_network, reason="Test only runs on HEAD network")
 def test_fetchone_axafocat_empty():
     s = Sqsh(server='sqlsao', user='aca_ops', database='axafocat')
     query = "select * from target where obsid=80000"
@@ -39,9 +47,24 @@ def test_fetchone_axafocat_empty():
     assert dat is None
 
 
+@pytest.mark.skipif(not on_head_network, reason="Test only runs on HEAD network")
 def test_fetchone_axafapstat_context():
     with Sqsh() as s:
         query = "select * from aspect_1 where obsid=5438 and revision=1"
         dat = s.fetchone(query)
         assert dat["obsid"] == 5438
         assert dat['ascdsver'] == '7.6.3'
+
+
+@pytest.mark.skipif(not on_head_network, reason="Test only runs on HEAD network")
+def test_no_passwd():
+    with pytest.raises(NoPasswordError):
+        s = Sqsh(server='sqlsao', user='aca_nonexistent', database='axafapstat')
+
+
+@pytest.mark.skipif(not on_head_network, reason="Test only runs on HEAD network")
+def test_fetch_axafapstat_lines():
+    s = Sqsh()
+    query = "select * from aspect_1 where obsid=5438"
+    lines = s.fetch(query)
+    assert len(lines) >= 5
